1b96145d823f8aac105110046f8aa0e56f3ab8e8
keziah55 <19816994+keziah55@users.noreply.github.com>
2026-02-24 21:34:07 +0000

    ctypes array from_buffer_copy

--- command.c
+++ command.c
@@ -2,26 +2,33 @@
 #include <stdio.h>
 
 struct CommandStruct {
-    uint8_t const size;
-    uint8_t const* command;
+  uint8_t const size;
+  uint8_t const *command;
 };
 
 struct DemoStruct {
-  uint8_t x;       
-  char c;   
+  uint8_t x;
+  char c;
 };
 
 void printDemoStruct(struct DemoStruct *);
 
+void printCommand(struct CommandStruct *);
+
 void printDemoStruct(struct DemoStruct *ds) {
-    printf("DemoStruct %d, %c\n", ds->x, ds->c);
+  printf("DemoStruct %d, %c\n", ds->x, ds->c);
+}
+
+void printCommand(struct CommandStruct *cs) {
+  for (uint8_t i = 0; i < cs->size; i++)
+    printf("Command: %d\n", *(cs + i));
 }
 
 int main() {
-    struct DemoStruct ds;
-    ds.x = 5;
-    ds.c = 'a';
-    printDemoStruct(&ds);
+  struct DemoStruct ds;
+  ds.x = 5;
+  ds.c = 'a';
+  printDemoStruct(&ds);
 
-    return 0;
+  return 0;
 }
--- command_wrap.py
+++ command_wrap.py
@@ -9,6 +9,10 @@ class DemoStruct(ctypes.Structure):
     _fields_ = [("x", ctypes.c_uint8), ("c", ctypes.c_char)]
 
 
+class CommandStruct(ctypes.Structure):
+    _fields_ = [("size", ctypes.c_uint8), ("command", ctypes.POINTER(ctypes.c_uint8))]
+
+
 if __name__ == "__main__":
 
     lib_path = Path().absolute().joinpath("libcommand.so")
@@ -19,3 +23,15 @@ if __name__ == "__main__":
     ds = DemoStruct(ctypes.c_uint8(10), ctypes.c_char(b"b"))
 
     libc.printDemoStruct(ctypes.byref(ds))
+
+    cmd = "1234"
+    cmd_bts = bytes.fromhex(cmd)
+    print(cmd_bts)
+
+    Array = ctypes.c_uint8 * len(cmd_bts)
+    ctypes_array = Array.from_buffer_copy(cmd_bts)
+    print(ctypes_array[0], ctypes_array[1])
+
+    cs = CommandStruct(ctypes.c_uint8(len(cmd_bts)), ctypes_array)
+
+    libc.printCommand(ctypes.byref(cs))
